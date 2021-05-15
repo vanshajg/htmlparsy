911ac25dc21b83bd80b38ce9bd0eabca76d04edd
Vanshaj Girotra <vanshaj.g@practo.com>
2021-05-16 01:27:41 +0530

    Add cache

--- server/go.mod
+++ server/go.mod
@@ -5,5 +5,6 @@ go 1.16
 require (
 	github.com/gin-gonic/contrib v0.0.0-20201101042839-6a891bf89f19
 	github.com/gin-gonic/gin v1.7.1
+	github.com/patrickmn/go-cache v2.1.0+incompatible
 	golang.org/x/net v0.0.0-20200202094626-16171245cfb2
 )
--- server/src/main.go
+++ server/src/main.go
@@ -1,8 +1,11 @@
 package main
 
 import (
+	"time"
+
 	"github.com/gin-gonic/contrib/static"
 	"github.com/gin-gonic/gin"
+	"github.com/patrickmn/go-cache"
 )
 
 func main() {
@@ -10,9 +13,9 @@ func main() {
 	r := gin.Default()
 	// serving client build files
 	r.Use(static.Serve("/", static.LocalFile("./web", true)))
-
+	apiCache := cache.New(5*time.Minute, 10*time.Minute)
 	api := r.Group("/api")
-	api.GET("/data", getData)
+	api.GET("/data", CacheCheck(apiCache), getData)
 
 	r.Run()
 }
