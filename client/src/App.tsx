67ab96bfe9a5a07f9d2825c6f4ebfa05c8b8138e
Vanshaj Girotra <vanshaj.g@practo.com>
2021-05-16 19:06:58 +0530

    Create frontend

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,17 +1,102 @@
-import { useEffect, useState } from 'react'
+import { useState } from 'react'
 import './App.css';
 import axios from 'axios';
+import { Input, Select, Flex, Checkbox, Button, Tooltip, FormControl, FormErrorMessage, Heading } from '@chakra-ui/react'
+import { AiOutlineInfoCircle } from 'react-icons/ai'
+import { Form, Formik, Field } from 'formik'
+import { TableData } from './Table'
 
-function App() {
-  const [api_data, setApiData] = useState("")
-  useEffect(() => { 
-    axios.get("/api/ping").then((response) => {
-      setApiData(response.data.message)
-    })
-  }, [])
+const validateUrl = (url: string): string => {
+  const regex = /(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)/
+  if (url === "")
+    return "url cannot be empty"
+  if (!regex.test(url))
+    return "url doesn't seem right"
+  return ""
+}
+
+const API_STATUS = {
+  "INIT": "INIT",
+  "FETCHING": "FETCHING",
+  "FETCHED": "FETCHED",
+  "FAILED": "FAILED",
+}
+
+const App = () => {
+  const [api_status, setApiStatus] = useState(API_STATUS.INIT)
+  const [api_data, setApiData] = useState({
+    "html_version": "",
+    "title": "",
+    "h1_count": 0,
+    "h2_count": 0,
+    "h3_count": 0,
+    "h4_count": 0,
+    "h5_count": 0,
+    "h6_count": 0,
+    "internal_links_count": 0,
+    "external_links_count": 0,
+    "inaccessible_links_count": 0,
+    "has_login_form": false
+  })
   return (
     <div className="App">
-     <h1>{api_data}</h1>
+      <Heading textAlign="center" mb="8vh">HTML Parser</Heading>
+      <Formik initialValues={{ url: "", protocol: "https", override_cache: false }} onSubmit={(values, actions) => {
+        setApiStatus(API_STATUS.FETCHING);
+        axios.get("/api/data", {
+          params: {
+            url: `${values.protocol}://${values.url}`,
+            ignoreCache: values.override_cache
+          }
+        }).then(data => {
+          setApiStatus(API_STATUS.FETCHED);
+          setApiData(data.data);
+        }).catch(err => {
+          setApiStatus(API_STATUS.FAILED);
+          actions.setFieldError("url", "Request for this URL failed, check the URL or try again");
+        }).finally(() => {
+          actions.setSubmitting(false)
+        })
+      }}>
+        {(props) => (
+          <Form style={{ display: "flex", justifyContent: "center", width: "100vw", height: "max-content", alignItems: "start" }}>
+            <Field name="protocol">
+              {({ field, form }: any) => (
+                <FormControl width="max-content">
+                  <Select size="lg" variant="filled" defaultValue="https" width="max-content" id="protocol" onChange={props.handleChange}>
+                    <option value="http">http://</option>
+                    <option value="https">https://</option>
+                  </Select>
+                </FormControl>
+              )}
+            </Field>
+            <Field name="url" validate={validateUrl}>
+              {({ field, form }: any) => (
+                <FormControl isRequired width="20vw" isInvalid={form.errors.url && form.touched.url}>
+                  <Input size="lg" {...field} id="url" placeholder="google.com" />
+                  <FormErrorMessage>{form.errors.url}</FormErrorMessage>
+                </FormControl>
+              )}
+            </Field>
+            <Button size="lg" colorScheme="blue" ml="4" type="submit" isLoading={props.isSubmitting}>Submit</Button>
+            <Field>
+              {() => (
+                <FormControl width="max-content">
+                  <Checkbox size="lg" ml="16" onChange={props.handleChange} id="override_cache">
+                    <Tooltip label="URL data and computation are cached for ~6hours, check this tickbox to flush cache for this url and recompute" placement="right" openDelay={500}>
+                      <Flex alignItems="center">
+                        Override cache?
+                       <AiOutlineInfoCircle style={{ marginLeft: "4px" }} />
+                      </Flex>
+                    </Tooltip>
+                  </Checkbox>
+                </FormControl>
+              )}
+            </Field>
+          </Form>
+        )}
+      </Formik>
+      {api_status === API_STATUS.FETCHED ? <TableData {...api_data} /> : null}
     </div>
   );
 }
---
+++ client/src/Table.tsx
@@ -0,0 +1,55 @@
+import {
+    Table,
+    Thead,
+    Tbody,
+    Tfoot,
+    Tr,
+    Th,
+    Td,
+} from "@chakra-ui/react"
+
+export const TableData = (props: any) => {
+    const { html_version, title, h1_count, h2_count, h3_count, h4_count,
+        h5_count, h6_count, internal_links_count, external_links_count,
+        inaccessible_links_count, has_login_form } = props;
+
+    const table_body = [
+        {key: "HTML version", val: html_version },
+        {key: "Page Title", val: title },
+        {key: "H1 count", val: h1_count},
+        {key: "H2 count", val: h2_count},
+        {key: "H3 count", val: h3_count},
+        {key: "H4 count", val: h4_count},
+        {key: "H5 count", val: h5_count},
+        {key: "H6 count", val: h6_count},
+        {key: "Internal Links Count", val: internal_links_count},
+        {key: "External Links Count", val: external_links_count},
+        {key: "Inaccessible Links Count", val: inaccessible_links_count},
+        {key: "Has Login Form", val: has_login_form ? "Yes" : "No"}
+    ]
+    return (
+        <Table variant="striped" colorScheme="gray" width="50vw" margin="auto" mt="36px" border="1px solid gray">
+            <Thead>
+                <Tr>
+                    <Th>Page Property</Th>
+                    <Th>Value</Th>
+                </Tr>
+            </Thead>
+            <Tbody>
+                {table_body.map(({key, val}, idx) => (
+                    <Tr key={idx}>
+                        <Td>{key}</Td>
+                        <Td>{val}</Td>
+                    </Tr>
+                ))}
+            </Tbody>
+            <Tfoot>
+                <Tr>
+                    <Th>Page Property</Th>
+                    <Th>Value</Th>
+                   
+                </Tr>
+            </Tfoot>
+        </Table>
+    )
+}
